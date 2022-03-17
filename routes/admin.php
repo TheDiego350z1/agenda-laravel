13aa034f5faebcc0c7be7928f7255c4f63f26991
Diego García <diego350z1@gmail.com>
2022-03-17 14:27:02 -0600

    Creación EventController

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\EventController;
 
 Route::get('', [HomeController::class, 'index']);
 
 Route::resource('/contacts', ContactController::class);
+Route::resource('/events', ContactController::class);
